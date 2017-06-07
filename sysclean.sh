58d74b024b27bf0df38f56cd732698d5049a3c2e
xlCHINCHlx <n50ghz@gmail.com>
2017-06-06 21:20:50 -0700

    . . . .

---
+++ sysclean.sh
@@ -0,0 +1,209 @@
+#!/system/bin/sh
+export PVAR=com.android.cts.priv.ctsshim
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.sdm.plugins.connmo
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.googlequicksearchbox
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.providers.calendar
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.providers.media
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.onetimeinitializer
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.htmlviewer
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.messaging
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.terminal
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=org.cyanogenmod.audiofx
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.cyanogenmod.setupwizard
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.vending
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.carrierconfig
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.contacts
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=org.lineageos.recorder
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.hotwordenrollment
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.egg
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.backupconfirm
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.cyanogenmod.lockclock
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=codeaurora.ims
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=org.cyanogenmod.cmaudio.service
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.sdm.plugins.sprintdm
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.calendar
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.setupwizard
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.qualcomm.qcrilmsgtunnel
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.sharedstoragebackup
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.printspooler
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.sdm.plugins.diagmon
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.dreams.basic
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.webview
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.cellbroadcastreceiver
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.onetimeinitializer
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.syncadapters.contacts
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.facelock
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.printservice.recommendation
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.gms
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.gsf
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.ims
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.tts
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.calllogbackup
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.partnersetup
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.svox.pico
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.feedback
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.syncadapters.calendar
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.managedprovisioning
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.dreams.phototable
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.sdm.plugins.dcmo
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.gsf.login
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.apps.tag
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.backuptransport
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.storagemanager
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.bookmarkprovider
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.calculator2
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.apps.tycho
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.cts.ctsshim
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.email
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.providers.userdictionary
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.emergency
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.location.fused
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.deskclock
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.exchange
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.huawei.hiddenmenu
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.gms.setup
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.google.android.apps.gcs
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.qualcomm.timeservice
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.qualcomm.atfwd
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.providers.contacts
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=org.cyanogenmod.weatherservice
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+export PVAR=com.android.captiveportallogin
+pm clear $PVAR;pm disable $PVAR;pm uninstall --user 0 $PVAR
+unset PVAR
+
